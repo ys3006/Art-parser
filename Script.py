bbc25b2ba12e812199616ce2dfc14f6bd7ce540d
ys3006 <31739475+ys3006@users.noreply.github.com>
2018-01-28 16:08:08 -0500

    test lot1.html

--- Script.py
+++ Script.py
@@ -2,3 +2,13 @@
 import bs4
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
+
+## test
+# try reading github file
+url = "https://raw.githubusercontent.com/ys3006/Art-parser/master/data/2015-03-18/lot1.html"
+
+# open the html, grabbing the page
+uOpen = uReq(url)
+page_html = uOpen.read()
+# close the connection
+uOpen.close()
